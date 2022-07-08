f837f98a65c4d7d1897f7b9e769618ff06447e8f
vrahmanov <vladi.rahmanov@locusview.com>
2022-07-08 10:46:40 +0300

    Eks initial creation with delta checks

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # ArgOps
+## Terraform
+````
+1 terraform init 
+2 terraform plan -var-file=fixtures.us-east-2.tfvars
+3 terraform apply -var-file=fixtures.us-east-2.tfvars
+4 terraform refresh -var-file=fixtures.us-east-2.tfvars  ( ONLY WHEN TIMED OUT) and then repeat step 3 
+
+````
---
+++ versions.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 0.13.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.72"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.10"
+    }
+     helm = {
+      source = "hashicorp/helm"
+      version = "2.6.0"
+    }
+  }
+}
+
+#terraform {
+#  required_providers {
+#    helm = {
+#      source = "hashicorp/helm"
+#      version = "2.6.0"
+#    }
+#  }
+#}
+#
+#provider "helm" {
+#  # Configuration options
+#}
